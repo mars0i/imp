a31925c24abed028c36c53b77846e70c3ad26368
mars0i <mabrams@uab.edu>
2017-05-06 16:50:48 -0500

    moved old defs to .x, fixed for new owl infix defs

--- doc/probmat.x
+++ doc/probmat.x
@@ -40,3 +40,36 @@ let rec subtract_list xs ys =
  * experimentation, I prefer this representation to using Batteries or Core
  * Sets, which don't display their contents by default.
 *)
+
+
+(** OLD DEFINITIONS OF pri_f_field_lowers/uppers  **)
+
+(** OLD Calculate a lower, L value from a probability interval.
+    Equation (3) p. 1317 in Skulj 2009 (funny capitalization evokes the paper) *)
+let pri_F_field_Lower  omega_max  atom_mins  atom_maxs  subset_idxs = 
+  let mins_sum = prob_sum atom_mins subset_idxs in
+  let maxs_comp_sum = prob_sum atom_maxs (list_complement omega_max subset_idxs) in
+  max mins_sum (1. -. maxs_comp_sum)
+
+(** OLD Calculate an upper, U value from a probability interval.
+    Equation (4) p. 1317 in Skulj 2009 *)
+let pri_F_field_Upper  omega_max  atom_mins  atom_maxs  subset_idxs = 
+  let maxs_sum = prob_sum atom_maxs subset_idxs in
+  let mins_comp_sum = prob_sum atom_mins (list_complement omega_max subset_idxs) in
+  min maxs_sum (1. -. mins_comp_sum)
+
+(** OLD Calculate L values for all members of the algebra and return an
+    (atoms, L-value) alist *)
+let pri_F_field_Lowers omega_max atom_mins atom_maxs =
+  let algebra_idx_sets = LL.at algebra_sets omega_max in
+  let lower idx_set =
+    (idx_set, pri_F_field_Lower omega_max atom_mins atom_maxs idx_set) in
+  L.map lower algebra_idx_sets
+
+(** OLD Calculate U values for all members of the algebra and return an
+    (atoms, U-value) alist *)
+let pri_F_field_Uppers omega_max atom_mins atom_maxs =
+  let algebra_idx_sets = LL.at algebra_sets omega_max in
+  let upper idx_set = 
+    (idx_set, pri_F_field_Upper omega_max atom_mins atom_maxs idx_set) in
+  L.map upper algebra_idx_sets
--- src/probmat.ml
+++ src/probmat.ml
@@ -8,13 +8,11 @@ module LL = Batteries.LazyList
 module A  = Batteries.Array
 (* module P = Pervasives *) (* to override defs from Owl.Mat *)
 
+
 (* Pull out infixes I might want so I don't have to open Owl.Mat and shadow Pervasives: *)
-let ($@) = Owl.Mat.($@)  (* = dot: matrix multiplication *)
-let (+@) = Owl.Mat.(+@) (* matrix addition *)
-let (-@) = Owl.Mat.(-@) (* matrix subtraction *)
-let (/$) = Owl.Mat.(/$) (* divide matrix by float *)
-let ($/) = Owl.Mat.($/) (* divide float by matrix *)
-let ( *@ ) = Owl.Mat.( *@ )  (* = mul: elementwise multiplication *)
+let ( *@ ) = M.( *@ )  (* = dot: matrix multiplication *)
+let (/$) = M.(/$) (* divide matrix by float *)
+
 
 let dec n = n - 1
 
@@ -193,37 +191,3 @@ let unif_stoch_mat dim =
 (** Make a square matrix initializing it with function f, which is
     passed indexes, e.g: mat_from_fn 4 (fun _ -> Owl.Stats.Rnd.uniform ())  *)
 let mat_from_fn dim f = M.map f (M.empty dim dim)
-
-
-
-(************** OLD **************)
-
-(** OLD Calculate a lower, L value from a probability interval.
-    Equation (3) p. 1317 in Skulj 2009 (funny capitalization evokes the paper) *)
-let pri_F_field_Lower  omega_max  atom_mins  atom_maxs  subset_idxs = 
-  let mins_sum = prob_sum atom_mins subset_idxs in
-  let maxs_comp_sum = prob_sum atom_maxs (list_complement omega_max subset_idxs) in
-  max mins_sum (1. -. maxs_comp_sum)
-
-(** OLD Calculate an upper, U value from a probability interval.
-    Equation (4) p. 1317 in Skulj 2009 *)
-let pri_F_field_Upper  omega_max  atom_mins  atom_maxs  subset_idxs = 
-  let maxs_sum = prob_sum atom_maxs subset_idxs in
-  let mins_comp_sum = prob_sum atom_mins (list_complement omega_max subset_idxs) in
-  min maxs_sum (1. -. mins_comp_sum)
-
-(** OLD Calculate L values for all members of the algebra and return an
-    (atoms, L-value) alist *)
-let pri_F_field_Lowers omega_max atom_mins atom_maxs =
-  let algebra_idx_sets = LL.at algebra_sets omega_max in
-  let lower idx_set =
-    (idx_set, pri_F_field_Lower omega_max atom_mins atom_maxs idx_set) in
-  L.map lower algebra_idx_sets
-
-(** OLD Calculate U values for all members of the algebra and return an
-    (atoms, U-value) alist *)
-let pri_F_field_Uppers omega_max atom_mins atom_maxs =
-  let algebra_idx_sets = LL.at algebra_sets omega_max in
-  let upper idx_set = 
-    (idx_set, pri_F_field_Upper omega_max atom_mins atom_maxs idx_set) in
-  L.map upper algebra_idx_sets
