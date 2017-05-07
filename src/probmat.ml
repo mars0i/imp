87dd4595953aafae05b8ed43ce5c615f59c6b0a0
mars0i <mabrams@uab.edu>
2017-05-06 23:40:49 -0500

    comments and clean-up

--- src/probmat.ml
+++ src/probmat.ml
@@ -44,20 +44,24 @@ let max_elts matlist = L.reduce M.max2 matlist
 (** length of a row vector or number of columns of a matrix *)
 let size m = snd (M.shape m)
 
-(** Given a sequence of sequences representing the power set of non-negative
-    integers up through n-1, returns a pair consisting of the sequence of 
-    sequences for up through n-1 and one up through n.  (This kind of pair 
-    is what LazyList.from_loop wants.)  Assumes that the first element of 
-    the first element of the power set sequence pset passed in is n-1. *)
+(** Create a power set of integers from a smaller power set.
+  *
+  * Given a sequence of sequences representing the power set of non-negative
+  * integers up through n-1, returns a pair consisting of the sequence of 
+  * sequences for up through n-1 and one up through n.  (This kind of pair 
+  * is what LazyList.from_loop wants.)  Assumes that the first element of 
+  * the first element of the power set sequence pset passed in is n-1. *)
 let next_intsets pset =
   let n = 1 + L.hd (L.hd pset) in  (* Get next integer; previous one must be first in the first element. *)
   let addl_sets = (L.map (fun xs -> n :: xs) pset) in
   (pset, addl_sets @ pset)
 
-(** Return a lazy list of subsequent power sets of integers from 0 to n. 
-    They can be retreived using e.g., to get the power set of integers
-    up to 5: LazyList.at intsets 4 .  Note each power set is in the form
-    of a regular list; only the top level list is lazy. *)
+(** Generate a list of subsequent integer power sets. 
+ *
+ *  Return a lazy list of subsequent power sets of integers from 0 to n. 
+ *  They can be retreived using e.g., to get the power set of integers
+ *  up to 5: LazyList.at intsets 4 .  Note each power set is in the form
+ *  of a regular list; only the top level list is lazy. *)
 let make_intsets () = LL.from_loop [[0]; []] next_intsets
 let algebra_sets = make_intsets ()
 
@@ -77,37 +81,36 @@ let algebra_probs probs =
   let idx_prob_entry idxs = (idxs, prob_sum probs idxs) in
   L.map idx_prob_entry idx_sets 
 
-(** Given two algebra_probs alists, return a similar alist in which values are 
+(** Given *two* algebra_probs alists, return a similar alist in which values are 
  * the minimum/maximum/etc (according to relat) of the two corresponding probs. *)
 let algebra_extrema relat alg_probs1 alg_probs2:((int list * float) list) =   (* compiler needs a little help with types here *)
   let make_entry (event, prob1) (_, prob2) =
     (event, relat prob1 prob2) in (* first elts s/b same *)
   L.map2 make_entry alg_probs1 alg_probs2
 
-(** Given two algebra_probs lists, return a similar list in which
+(** Given *two* algebra_probs lists, return a similar alist in which
     values are the minimum of the two corresponding probabilities. *)
 let algebra_mins = algebra_extrema min
 
-(** Given two algebra_probs lists, return a similar list in which
+(** Given *two* algebra_probs lists, return a similar alist in which
     values are the maximum of the two corresponding probabilities. *)
 let algebra_maxs = algebra_extrema max
 
-(** Given a list of algebra_probs, return an algebra_prob-like list
-    with minima of all probs for each set of indexes. *)
+(** Given a list of *multiple* algebra_probs, return an algebra_prob-like
+    list with minima of all probs for each set of indexes. *)
 let min_algebra_elts alg_probs_list =
   L.reduce (fun combo alg -> algebra_mins combo alg) alg_probs_list
 
-(** Given a list of algebra_probs, return an algebra_prob-like list
-    with maxima of all probs for each set of indexes. *)
+(** Given a list of *multiple* algebra_probs, return an algebra_prob-like
+    list with maxima of all probs for each set of indexes. *)
 let max_algebra_elts alg_probs_list =
   L.reduce (fun combo alg -> algebra_maxs combo alg) alg_probs_list
 
 (** Given lists xs and ys that are both ordered in the same way (e.g. 
     monotonically ordered integers), return a list containing all
     elements of xs not in ys (with order preserved).  The elements
-    in ys must be a (perhaps improper) subset of xs. *)
-(* This version is by RichN at https://codereview.stackexchange.com/a/162407/61384
- * as a code review answer to my original version. *)
+    in ys must be a (perhaps improper) subset of xs.
+    This version by RichN at https://codereview.stackexchange.com/a/162407/61384 *)
 let rec subtract_list xs ys =
   match xs, ys with 
   | [], _ -> []
@@ -115,36 +118,35 @@ let rec subtract_list xs ys =
   | x::xs', y::ys' when x = y -> subtract_list xs' ys'
   | x::xs', _ -> x::(subtract_list xs' ys)
 
-(** Given a maximum element omega_max and a subset of a domain of atoms 
-    represented by list of integers in decreasing order, return a list
-    of the integers, between 0 and omega_max inclusive, that are not in
-    the subset.  The returned list will also be in decreasing order.
-    Note that omega_max is one less than the size of the domain. *)
+(** Return the complement of a list of atoms--i.e. return the atoms
+  * representing the negation of the original set.
+  *
+  * Given a maximum element omega_max and a subset of a domain of atoms 
+  * represented by list of integers in decreasing order, return a list
+  * of the integers, between 0 and omega_max inclusive, that are not in
+  * the subset.  The returned list will also be in decreasing order.
+  * Note that omega_max is one less than the size of the domain. *)
 let list_complement omega_max subset =
   let omega = L.range omega_max `Downto 0 in
   subtract_list omega subset
 
-(* let algebra_complements omega idx_sets = L.map (fun s -> subtract_list omega s) idx_sets *)
-
-(* DEBUG *)
-let prmax x y = 
-  let mx = max x y in
-  Printf.printf "%s\n" (if mx = x then "first" else "second");
-  mx
-
+(** Return 1 - the sum of probs of extreme probs for a set of atoms
+    [for (3) and (4) in Skulj] *)
 let invert_prob_sum omega_max atom_extrema subset_idxs = 
   1. -. prob_sum atom_extrema (list_complement omega_max subset_idxs)
 
-(** THESE ARE NOT RIGHT.  I'm getting numbers greater than 1 and less than zero! **)
-
+(** Map prob_sum over each possible combination of atoms. *)
 let pri_f_field_simple_sums omega_max atom_extrema =
   let algebra_idx_sets = LL.at algebra_sets omega_max in
   L.map (prob_sum atom_extrema) algebra_idx_sets
 
+(** Map invert_prob_sum over each possible combination of atoms. *)
 let pri_f_field_inverted_sums omega_max atom_extrema =
   let algebra_idx_sets = LL.at algebra_sets omega_max in
   L.map (invert_prob_sum omega_max atom_extrema) algebra_idx_sets
 
+(** Calculate L values for all members of the algebra and return an
+    (atoms, L-value) alist *)
 let pri_f_field_lowers omega_max atom_mins atom_maxs =
   let algebra_idx_sets = LL.at algebra_sets omega_max in
   let mins = pri_f_field_simple_sums omega_max atom_mins in
@@ -152,6 +154,8 @@ let pri_f_field_lowers omega_max atom_mins atom_maxs =
   let minmins = L.map2 max mins inverted_maxs in
   L.combine algebra_idx_sets minmins
 
+(** Calculate U values for all members of the algebra and return an
+    (atoms, U-value) alist *)
 let pri_f_field_uppers omega_max atom_mins atom_maxs =
   let algebra_idx_sets = LL.at algebra_sets omega_max in
   let maxs = pri_f_field_simple_sums omega_max atom_maxs in
@@ -159,6 +163,9 @@ let pri_f_field_uppers omega_max atom_mins atom_maxs =
   let maxmaxs = L.map2 min maxs inverted_mins in
   L.combine algebra_idx_sets maxmaxs
 
+(** Given lists of atoms, value pairs from pri_f_field_lowers and
+  * pri_f_field_uppers, return a list of pairs that combine the lower
+  * and upper values into intervals represented as 2-element lists. *)
 let pri_f_field_intervals lowers uppers =
   let add_elt elts (event, lower_prob) (_, upper_prob) =   (* events s/b same in lower and upper *)
     (event, [lower_prob; upper_prob]) :: elts
