7d3f5307e296dcb2a1688385c9c75c3f2277ef81
mars0i <mabrams@uab.edu>
2017-05-17 01:31:58 -0500

    Functions for intervals between matrices and vectors
    e.g. special defined compare function that refuses to return
    -1 or 1 if there are elementwise comparisons that point in
    opposite directions.  Throws an exception in that case.

---
+++ src/interval.ml
@@ -0,0 +1,30 @@
+
+module M  = Owl.Mat
+module I = Core.Interval;;
+
+(* TODO is this right? *)
+(** Return true iff pred is true for all corresponding elements of
+    matrices m1 and m2. Short-circuits on the first false. *)
+let forall2 pred m1 m2 =
+  let rows, cols as dims = M.shape m1 in
+  if dims <> M.shape m2 then raise (Failure "matrices have different shapes");
+  let rec loop_cols i j =
+    if j >= cols then true else
+      pred (M.get m1 i j) (M.get m2 i j) && loop_cols i (j + 1)
+  in let rec loop_rows i j =
+    if i >= rows then true else
+      loop_cols i j && loop_rows (i + 1) j
+  in loop_rows 0 0
+
+(** Compare function for matrices that returns zero of all elements of
+    both matrices are equal, and if not returns -1 only if the matrices are 
+    not equal and all elements of m1 are less than or equal to corresponding 
+    elements of m2, and if not returns 1 only if all elements of m1 are 
+    greater than or equal to corresponding elements of m2, and raises an
+    exception of none of these relationships hold. (The normal compare
+    function short-circuits on the first non-equal elements.) *)
+let compare m1 m2 =
+  if m1 = m2 then 0 else
+    if forall2 (<=) m1 m2 then -1 else 
+      if forall2 (>=) m1 m2 then 1 
+      else raise (Failure "incomparable matrices")
