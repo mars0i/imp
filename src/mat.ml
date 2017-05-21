e0a3cd2729eacf0622fca3eafd28b535e266bd43
mars0i <mabrams@uab.edu>
2017-05-20 23:11:25 -0500

    attempting to RWO-style Intervals for owl matrices

---
+++ src/mat.ml
@@ -0,0 +1,25 @@
+module M  = Owl.Mat
+(* module I = Core.Interval;; *)
+
+(* Something like this is what one wants, I think:
+module Mat_interval = Interval.Make(struct 
+  type t = Owl.Mat.mat
+  let compare = Comparemat.compare
+end);;
+*)
+
+type t = Owl.Mat.mat
+
+(** A compare function for matrices that returns zero if all elements of
+    both matrices are equal, and if not returns -1 only if all elements 
+    of m1 are less than or equal to corresponding elements of m2; otherwise
+    returns 1, indicating that at least one element in m1 is greater than 
+    the corresponding element in m2. *)
+let compare m1 m2 =
+  let f acc e1 e2 =
+    if acc = 1 || e1 > e2 then 1  (* at least one pair has had e1 > e2 *)
+    else match acc with           (* at this point we know that e1 <= e2 *)
+         | -1 -> -1               (* all previous pairs were <= *)
+         |  0 -> if e1 = e2 then 0 else -1
+         |  _ -> raise (Failure "bug in compare: acc is not -1, 0, or 1")
+  in Utils.fold2 f 0 m1 m2
--- src/utils.ml
+++ src/utils.ml
@@ -1,15 +1,5 @@
-
 module M  = Owl.Mat
-(* module I = Core.Interval;; *)
 
-(* Something like this is what one wants, I think:
-module Mat_interval = Interval.Make(struct 
-  type t = Owl.Mat.mat
-  let compare = Comparemat.compare
-end);;
-*)
-
-(* TODO is this right? *)
 (** Return true iff pred is true for all corresponding elements of
     matrices m1 and m2. Short-circuits on the first false. *)
 let forall2 pred m1 m2 =
@@ -39,17 +29,3 @@ let fold2 f init m1 m2 =
          then loop acc 0 (j + 1) (* start over on next col *)
          else acc
   in loop init 0 0
-
-(** A compare function for matrices that returns zero if all elements of
-    both matrices are equal, and if not returns -1 only if all elements 
-    of m1 are less than or equal to corresponding elements of m2; otherwise
-    returns 1, indicating that at least one element in m1 is greater than 
-    the corresponding element in m2. *)
-let compare m1 m2 =
-  let f acc e1 e2 =
-    if acc = 1 || e1 > e2 then 1  (* at least one pair has had e1 > e2 *)
-    else match acc with           (* at this point we know that e1 <= e2 *)
-         | -1 -> -1               (* all previous pairs were <= *)
-         |  0 -> if e1 = e2 then 0 else -1
-         |  _ -> raise (Failure "bug in compare: acc is not -1, 0, or 1")
-  in fold2 f 0 m1 m2
