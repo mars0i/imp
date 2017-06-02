9a743c5c1168b619e166ad19b0cec89628f560f3
mars0i <mabrams@uab.edu>
2017-06-01 22:43:22 -0500

    added comments, formatted some code

--- src/markov1.ml
+++ src/markov1.ml
@@ -1,8 +1,17 @@
+(* experiments with pop freq (normal) Markov procs *)
+
+(* Note that owl comes with a built-in plot module that can make 3D
+ * wireframe plots and countour heatmaps as well as various 2D plots. *)
+
 module LL = Batteries.LazyList;;
 
 let s = P.vec_from_int_list [0; 0; 0; 1; 0;]
 
-let t = P.mat_from_lists [[1.; 0.; 0.; 0.; 0.]; [0.5; 0.3; 0.2; 0.; 0.]; [0.; 0.5; 0.3; 0.2; 0.]; [0.; 0.; 0.5; 0.3; 0.2]; [0.; 0.; 0.; 0.; 1.]]
+let t = P.mat_from_lists [[1.; 0.; 0.; 0.; 0.];
+                          [0.5; 0.3; 0.2; 0.; 0.];
+                          [0.; 0.5; 0.3; 0.2; 0.];
+                          [0.; 0.; 0.5; 0.3; 0.2];
+                          [0.; 0.; 0.; 0.; 1.]]
 
 let next v = M.(v *@ t)
 
