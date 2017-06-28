3606f4a6a319c47bfef6dd7ef4e9d10ba834ff5a
mars0i <mabrams@uab.edu>
2017-06-28 13:30:18 -0500

    working on true vertices function

--- src/setchains.ml
+++ src/setchains.ml
@@ -1,8 +1,9 @@
-(** Functions inspired by Hartfiel's _Markov Set-Chains_, Springer 1998 *)
+(** Functions inspired by Hartfiel's _Markov Set-Chains_, Springer 1998.
+    Please see this book for definitions of terms, proofs, algorithms. *)
 
 module L = Batteries.List
 module A = Batteries.Array
-module M = Owl.Mat
+module M = Owl.M
 
 (** Given two lists of length n, return a list containing the 2^n lists
     containing each combination of elements from p and q at the same indices.
@@ -11,7 +12,7 @@ module M = Owl.Mat
     returns
        [[1; 2; 3];   [1; 2; 300];   [1; 200; 3];   [1; 200; 300];
         [100; 2; 3]; [100; 2; 300]; [100; 200; 3]; [100; 200; 300]] *)
-let rec vertices p q =
+let rec sequences p q =
   match p, q with
   | [], [] -> []
   | hp::[], hq::[] -> [[hp];[hq]]
@@ -20,8 +21,26 @@ let rec vertices p q =
       L.concat [L.map (L.cons hp) tailverts; L.map (L.cons hq) tailverts]
   | _, _ -> raise (Failure "lists are not the same length")
 
-(* No doubt there's some slightly better way to do this. *)
-let mat_to_lists m = L.map A.to_list (A.to_list (A.map M.to_array (M.to_rows m)))
+(** Converts a 1xN matrix, i.e. row vector, into a list. *)
+let vec_to_list m = A.to_list (M.to_array m)
+
+(** Converts an MxN matrix into a list of M lists of length N. *)
+let mat_to_lists m = A.to_list (A.map (A.to_list % M.to_array) (M.to_rows m))
+  
+(** Given the vector boundaries of an interval, lists its vertices assuming 
+    it is tight. *)
+let vertices p q =
+  let (_, size) = M.shape p in
+  let pl, ql = mat_to_lists p, mat_to_lists q in
+  let vertices_at i pl ql =
+    let pl', ql' = L.remove_at i pl, L.remove_at ql in
+    let seqs = sequences pl' ql' in
+    let sums = L.map (fun seq -> 1 - (L.fsum seq)) seqs in
+    (* now take the ones that are nonnegative, and use them to select
+     * the sequences that can be made stochastic, and then insert the
+     * nonnegative value into the ith location in the sequence, and
+     * ultimately make that into a vector. *)
+
 
 (** Can also be used with vectors. *)
 let mat_vertices m1 m2 =
