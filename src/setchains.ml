82cc1ac309f9ca15ff04cdff35715bfc2a02be45
mabrams <mabrams@uab.edu>
2017-06-28 18:06:11 -0500

    more work toward true vertices function

--- src/setchains.ml
+++ src/setchains.ml
@@ -3,7 +3,7 @@
 
 module L = Batteries.List
 module A = Batteries.Array
-module M = Owl.M
+module M = Owl.Mat
 
 (** Given two lists of length n, return a list containing the 2^n lists
     containing each combination of elements from p and q at the same indices.
@@ -17,7 +17,7 @@ let rec sequences p q =
   | [], [] -> []
   | hp::[], hq::[] -> [[hp];[hq]]
   | hp::tp, hq::tq -> 
-      let tailverts = vertices tp tq in
+      let tailverts = sequences tp tq in
       L.concat [L.map (L.cons hp) tailverts; L.map (L.cons hq) tailverts]
   | _, _ -> raise (Failure "lists are not the same length")
 
@@ -25,28 +25,29 @@ let rec sequences p q =
 let vec_to_list m = A.to_list (M.to_array m)
 
 (** Converts an MxN matrix into a list of M lists of length N. *)
-let mat_to_lists m = A.to_list (A.map (A.to_list % M.to_array) (M.to_rows m))
+let mat_to_lists m =
+  let open Batteries in
+  A.to_list (A.map (A.to_list % M.to_array) (M.to_rows m))
+
+let insert_after n new_elt l = 
+  if n = 0 then new_elt::l else
+  L.fold_righti 
+    (fun i elt acc -> if n = i then elt::new_elt::acc else elt::acc)
+    l []
+
   
 (** Given the vector boundaries of an interval, lists its vertices assuming 
     it is tight. *)
-let vertices p q =
-  let (_, size) = M.shape p in
-  let pl, ql = mat_to_lists p, mat_to_lists q in
-  let vertices_at i pl ql =
-    let pl', ql' = L.remove_at i pl, L.remove_at ql in
-    let seqs = sequences pl' ql' in
-    let sums = L.map (fun seq -> 1 - (L.fsum seq)) seqs in
-    (* now take the ones that are nonnegative, and use them to select
-     * the sequences that can be made stochastic, and then insert the
-     * nonnegative value into the ith location in the sequence, and
-     * ultimately make that into a vector. *)
+let list_vertices p q =
+  let vertices_at i p q =
+    let p', q' = L.remove_at i p, L.remove_at i q in
+    let seqs = sequences p' q' in
+    let sums = L.map (fun seq -> 1. -. (L.fsum seq)) seqs in
+    let add_vertex sum seq acc =
+      if sum > 0. then (insert_after i sum seq)::acc
+      else acc
+    in L.fold_right2 add_vertex sums seqs []
+  in
+  L.concat (L.map2i vertices_at p q)
 
-
-(** Can also be used with vectors. *)
-let mat_vertices m1 m2 =
-  let m1_list, m2_list = mat_to_lists m1, mat_to_lists m2 in
-  L.map2 vertices m1_list m2_list
-
-
-
-
+let mat_vertices p q = list_vertices (mat_to_lists p) (mat_to_lists q)
