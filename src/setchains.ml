aeb691a46a22128935ad40426a210443acc0b2bd
mars0i <mabrams@uab.edu>
2017-06-27 22:22:32 -0500

    added initial version of function to get vertices from a vector or matrix

--- src/setchains.ml
+++ src/setchains.ml
@@ -2,6 +2,7 @@
 
 module L = Batteries.List
 module A = Batteries.Array
+module M = Owl.Mat
 
 (** Given two lists of length n, return a list containing the 2^n lists
     containing each combination of elements from p and q at the same indices.
@@ -18,3 +19,15 @@ let rec vertices p q =
       let tailverts = vertices tp tq in
       L.concat [L.map (L.cons hp) tailverts; L.map (L.cons hq) tailverts]
   | _, _ -> raise (Failure "lists are not the same length")
+
+(* No doubt there's some slightly better way to do this. *)
+let mat_to_lists m = L.map A.to_list (A.to_list (A.map M.to_array (M.to_rows m)))
+
+(** Can also be used with vectors. *)
+let mat_vertices m1 m2 =
+  let m1_list, m2_list = mat_to_lists m1, mat_to_lists m2 in
+  L.map2 vertices m1_list m2_list
+
+
+
+
