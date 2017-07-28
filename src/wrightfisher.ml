a351e9da620e8fbfc573845086433cfaa2f9bd01
mars0i <mabrams@uab.edu>
2017-07-28 13:41:25 -0500

    added new set_ydigits kludge

--- src/wrightfisher.ml
+++ src/wrightfisher.ml
@@ -205,6 +205,7 @@ let add_3D_plot ?plot_max ?fontsize h altitude azimuth xs ys zs =
 
 (* Kludge to allow running with vanilla Owl that doesn't include it: *)
 (* let set_ydigits h n = () *)
+let set_ydigits h n = Plplot.plsyax n 0
 
 (** Add a single 2D plot to handle h. To be used with make_pdfs.  *)
 let add_2D_plot ?plot_max ?fontsize h ys zs =
