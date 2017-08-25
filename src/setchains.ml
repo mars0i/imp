593d734d45be2292793dba0d2dbfdb9e85a696d6
mars0i <mabrams@uab.edu>
2017-08-24 23:47:40 -0500

    commented out example 2.10 definitions

--- src/setchains.ml
+++ src/setchains.ml
@@ -229,7 +229,7 @@ let recombine_p l p q =
 
 (************************************************************)
 (** Example 2.10 *)
-
+(*
 let mp  = M.of_array [|0.35; 0.55; 0.25; 0.65|] 2 2;;
 let mq  = M.of_array [|0.45; 0.65; 0.35; 0.75|] 2 2;;
 let s0p = M.of_array [|0.4; 0.5|] 1 2;;
@@ -244,4 +244,5 @@ let w1 = Pm.cross_apply M.dot s0_verts m_verts;;  (* dot = ( *@ )  *)
 let s1_verts = [(L.last w1); (L.first w1)];;  (* (0.29, 0.71), (0.4, 0.6); see Hartfiel *)
 
 let w2 = Pm.cross_apply M.dot s1_verts m_verts;;
+*)
 
