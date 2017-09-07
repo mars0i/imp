4365aa872d8fe0bff4d7aea63b6a66f681e6e57f
mars0i <mabrams@uab.edu>
2017-09-06 21:47:56 -0500

    added commandline argument

--- src/bin/setchaintest.ml
+++ src/bin/setchaintest.ml
@@ -2,24 +2,37 @@ module W = Wf.Wrightfisher
 module U = Matutils.Utils
 module S = Wf.Setchains
 
-let n = 1000;;
+module Command = Core.Command
+module Spec = Core.Command.Spec
 
-Printf.printf "N=%d\n" n;;
+let run_test n () =
+  Printf.printf "N=%d\n" n;
+  print_string "make initial interval: ";
+  let p', q' = W.(U.time3 S.make_wf_interval n {w11=1.0; w12=0.3; w22=0.1}
+                                               {w11=1.0; w12=0.9; w22=0.5}) in
+  print_string "tighten it: ";
+  let p, q = U.time2 S.tighten_mat_interval p' q' in
+  print_string "calculate lo2, hi2: ";
+  let lo2, hi2 = Matutils.Utils.time3 S.make_kth_bounds_mats p q 2 
+  in (fun _ _ -> ()) lo2 hi2  (* get rid of warnings *)
 
-let p', q' = W.(S.make_wf_interval n {w11=1.0; w12=0.3; w22=0.1} {w11=1.0; w12=0.9; w22=0.5});;
 
-print_string "tighten: ";;
-let p, q = U.time2 S.tighten_mat_interval p' q';;
+let description = "time setchain functions";;
 
+let commandline = Command.basic ~summary:description
+                                ~readme:(fun () -> description)
+                                Spec.(empty +> anon ("N" %: int))
+                                run_test;;
+
+let () = Command.run ~version:"1.0"
+                     ~build_info:"setchaintest, (c) 2017 Marshall Abrams" 
+                     commandline
+
+(*
 let next_bounds_test mk_bounds p_mat q_mat = 
   mk_bounds S.recombine_lo p_mat q_mat p_mat,
   mk_bounds S.recombine_hi p_mat q_mat q_mat;;
 
-
-print_string "current default lo, hi: ";;
-let lo2, hi2 = Matutils.Utils.time3 S.make_kth_bounds_mats p q 2;;
-
-(*
 print_string "mat1: ";;
 let lo2', hi2' = Matutils.Utils.time3 next_bounds_test S.make_bounds_mat1 p q in
 Printf.printf "lo: %B\n" (lo2 = lo2');
