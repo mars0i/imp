229d0648193cd5179956ce0825e85980292eecfc
mabrams <mabrams@uab.edu>
2017-09-08 21:13:51 -0500

    commented out mat1 version

--- src/bin/setchaintest.ml
+++ src/bin/setchaintest.ml
@@ -8,7 +8,7 @@ module Command = Core.Command
 module Spec = Core.Command.Spec
 
 let run_test n () =
-  Printf.printf "N=%d\n\n" n;
+  Printf.printf "N=%d\n\n%!" n;
   print_string "make initial interval: ";
   let p', q' = W.(U.time3 S.make_wf_interval n {w11=1.0; w12=0.3; w22=0.1}
                                                {w11=1.0; w12=0.9; w22=0.5}) in
@@ -25,14 +25,14 @@ let run_test n () =
   let lo2', hi2' = Matutils.Utils.time3 Slow3.make_kth_bounds_mats p q 2  in
   let noparmap_duration = Unix.gettimeofday() -. noparmap_start_time in
 
-  Printf.printf "\nResults of both calculations are the same? %B\n" ((lo2, hi2) = (lo2', hi2'));
-  Printf.printf "Ratio between make_bounds_mat4 and make_bounds_mat3 wall times ~= %f\n"
+  Printf.printf "\nResults of both calculations are the same? %B\n%!" ((lo2, hi2) = (lo2', hi2'));
+  Printf.printf "Ratio between make_bounds_mat4 and make_bounds_mat3 wall times ~= %f\n%!"
                 (noparmap_duration /. parmap_duration);
-
+  (*
   print_string "\nExtra check: calculate lo2, hi2 without Parmap using (earlier) make_bounds_mat1: ";
   let lo2'', hi2'' = Matutils.Utils.time3 Slow1.make_kth_bounds_mats p q 2  in
   Printf.printf "Results are the same? %B\n" ((lo2, hi2) = (lo2'', hi2''));
-
+  *)
   print_string "\nOverall time:\n"
 
 
