427ceb72652786c806d0b1dc16f36e9f41894a84
mars0i <mabrams@uab.edu>
2017-09-16 23:29:18 -0500

    moved non-wrightfisher tranmat code to new file

---
+++ src/lib/models/tranmats.ml
@@ -0,0 +1,46 @@
+(** Tranmats: Lists, Lazy Lists, and other structures containing
+    transition matrices or vectors produced by them.  Agnostic about
+    the source of the matrices.  For example, there's nothing here about
+    fitnesses. *)
+
+module Mat = Owl.Mat
+module L = Batteries.List
+module LL = Batteries.LazyList
+
+let ( *@ ) = Mat.( *@ )  (* = dot: matrix multiplication *)
+
+(** Return pair of old distribution vector (i.e. the second argument) and
+    distribution vector (i.e. the product of the two arguments), for use 
+    as an element in a Batteries.LazyList *)
+let next_dist tranmat dist = 
+  (dist, dist *@ tranmat)
+
+(** Returns a LazyList of probability distributions that are the result of 
+    a stationary Markov chain with initial distribution (usually with 1 at 
+    one entry and zero elsewhere, representing that the population has that 
+    initial frequency) and a transition matrix. The initial distribution is 
+    included in the list.*)
+let make_dists tranmat init_dist =
+  LL.from_loop init_dist (next_dist tranmat)
+
+(** Given a list of transition matrices, mats, and a list of
+    probability distributions, dists, returns a new list of
+    probability distributions produced by multiplying all
+    distributions by all matrices. *)
+let next_dists tranmats dists =
+  (dists, L.concat (L.map (fun dist -> L.map (Mat.dot dist) tranmats)
+                          dists))
+
+(** Given a list of transition matrices and a list of initial distributions
+    (often one distribution with all weight on one frequency),
+    return a lazy list of lists of probability distributions.
+    Note this function does not not drop the first element. That way, the 
+    number of dists in the nth distlist = (length tranmats)**n for one 
+    initial distribution.  e.g. with
+    two transition matrices and one initial distribution,
+       LL.at distlists 1
+    will produce 2**1 = 2 dists.  Or with more initial distributions, the
+    number of dists at n is (length init_dists) * (length tranmats)**n . *)
+let make_distlists_from_mats tranmats init_dists =
+  LL.from_loop init_dists (next_dists tranmats)
+
--- src/lib/models/wrightfisher.ml
+++ src/lib/models/wrightfisher.ml
@@ -11,8 +11,7 @@ module Prob = Owl.Stats.Pdf
 module L = Batteries.List
 module LL = Batteries.LazyList
 module U = Utils.Genl
-
-let ( *@ ) = Mat.( *@ )  (* = dot: matrix multiplication *)
+module T = Tranmats
 
 let make_init_dist allele_popsize a1count =
   let m = Mat.zeros 1 (allele_popsize + 1) in
@@ -45,41 +44,6 @@ let make_tranmat allele_popsize fitns =
   let m = Mat.empty dim dim  in
   Mat.mapi (prob_ij_ fitns allele_popsize) m
 
-(** Return pair of old distribution vector (i.e. the second argument) and
-    distribution vector (i.e. the product of the two arguments), for use 
-    as an element in a Batteries.LazyList *)
-let next_dist tranmat dist = 
-  (dist, dist *@ tranmat)
-
-(** Returns a LazyList of probability distributions that are the result of 
-    a stationary Markov chain with initial distribution (usually with 1 at 
-    one entry and zero elsewhere, representing that the population has that 
-    initial frequency) and a transition matrix. The initial distribution is 
-    included in the list.*)
-let make_dists tranmat init_dist =
-  LL.from_loop init_dist (next_dist tranmat)
-
-(** Given a list of transition matrices, mats, and a list of
-    probability distributions, dists, returns a new list of
-    probability distributions produced by multiplying all
-    distributions by all matrices. *)
-let next_dists tranmats dists =
-  (dists, L.concat (L.map (fun dist -> L.map (Mat.dot dist) tranmats)
-                          dists))
-
-(** Given a list of transition matrices and a list of initial distributions
-    (often one distribution with all weight on one frequency),
-    return a lazy list of lists of probability distributions.
-    Note this function does not not drop the first element. That way, the 
-    number of dists in the nth distlist = (length tranmats)**n for one 
-    initial distribution.  e.g. with
-    two transition matrices and one initial distribution,
-       LL.at distlists 1
-    will produce 2**1 = 2 dists.  Or with more initial distributions, the
-    number of dists at n is (length init_dists) * (length tranmats)**n . *)
-let make_distlists_from_mats tranmats init_dists =
-  LL.from_loop init_dists (next_dists tranmats)
-
 (** Like make_distlists_from_mats, but uses basic parameters to generate the 
     transition matrices and initial distributions that are arguments to 
     make_distslists.  Arg 1, size is the number of alleles, i.e. 2N; arg 2,
@@ -89,7 +53,7 @@ let make_distlists_from_mats tranmats init_dists =
 let make_distlists size init_freqs fitn_list =
   let init_dists = L.map (make_init_dist size) init_freqs in
   let tranmats = L.map (make_tranmat size) fitn_list in
-  make_distlists_from_mats tranmats init_dists
+  T.make_distlists_from_mats tranmats init_dists
 
 (** Given a list of float fitness values, which should be in the order
        w11, w12, w22, w11, w12, w22, ...
