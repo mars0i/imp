f07d5b6a6268058ae1286a712f874f846780009e
mars0i <mabrams@uab.edu>
2018-01-05 22:41:55 -0600

    modified make_setchain_bounds_pdfs for new make_pdfs

--- src/lib/models/credalsetIO.ml
+++ src/lib/models/credalsetIO.ml
@@ -250,7 +250,7 @@ let make_setchain_bounds_pdfs ?(addl_2D_fn=fill_bounds)
                               ?(colors=Pl.[RGB (0,0,200); RGB (200,0,0)])
                               ?(leftright=true) ?(rows=1) ?(cols=1) ?(every=1)
                               ?plot_max ?fontsize 
-                              basename start_gen last_gen distlists =
+                              basename distlists =
   make_pdfs ~pdfdim:TwoD ~addl_2D_fn ~colors
             ~leftright ~rows ~cols ?plot_max ?fontsize
             basename distlists
