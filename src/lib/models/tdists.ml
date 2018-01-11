2f7c8c2f29cc68ba2c51c1feab97eb03c8c9aae7
mabrams <mabrams@uab.edu>
2018-01-11 16:11:01 -0600

    remaining part of split tdist/tranmats; added select fn

---
+++ src/lib/models/tdists.ml
@@ -0,0 +1,41 @@
+module Mat = Owl.Mat
+module LL = Batteries.LazyList
+
+let always_true _ = true
+
+type t = {gen : int ; dists : Mat.mat list}
+(** accessor, constructor functions: *)
+let gen tds = tds.gen
+let dists tds = tds.dists
+let make_tdists gen dists = {gen ; dists}
+
+(* Transform lazy lists of dists to/from lazy lists of tdists: *)
+let ints_from n = LL.seq n ((+) 1) always_true
+
+let add_gens ?(first_tick=0) dists_llist =
+  LL.map2 make_tdists (ints_from first_tick) dists_llist
+
+let remove_gens tdists_llist = LL.map dists tdists_llist
+
+(** [tdists_sublist start_t finish_t tdists_llist] returns a lazy list that's
+    a sublist of [tdists_llist], from the element with [gen]=[start_gen] to the 
+    element with [gen]=[finish_gen], inclusive.  Note that if the list is infinite
+    and there is no element with [gen]=[start_gen] or with [gen]=[finish_gen], the 
+    function will run forever, or until the system is overloaded. *)
+let sublist start_gen finish_gen tdists_llist =
+  LL.take_while (fun tds -> tds.gen <= finish_gen)
+                (LL.drop_while (fun tds -> tds.gen < start_gen)
+		               tdists_llist)
+
+let selection generations tdists_llist =
+  let add_if_selected td gens_plus_tds =
+	let gens, tds = gens_plus_tds in
+	if td.t = (L.hd generations)
+        then (L.tl generations), (LL.cons td tds)
+	else generations, tds
+  in
+  let empty_generations, new_tdists_llist = 
+	LL.fold_right add_if_selected (generations, LL.nil) tdists_llist in
+  new_tdists_llist
+
+    
