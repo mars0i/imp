3a9d8f5925c553163f71d2e7302ca628966b5b74
mabrams <mabrams@uab.edu>
2018-01-11 23:38:40 -0600

    working version of select_gens. deleted obsolute fn selection.

--- src/lib/models/tdists.ml
+++ src/lib/models/tdists.ml
@@ -19,34 +19,28 @@ let add_gens ?(first_tick=0) dists_llist =
 let remove_gens tdists_llist = LL.map dists tdists_llist
 
 (** [tdists_sublist start_t finish_t tdists_llist] returns a lazy list that's
-    a sublist of [tdists_llist], from the element with [gen]=[start_gen] to the 
-    element with [gen]=[finish_gen], inclusive.  Note that if the list is infinite
-    and there is no element with [gen]=[start_gen] or with [gen]=[finish_gen], the 
-    function will run forever, or until the system is overloaded. *)
+    a finite sublist of [tdists_llist], from the element with [gen]=[start_gen]
+    to the element with [gen]=[finish_gen], inclusive.  Note that if the list
+    is infinite and there is no element with [gen]=[start_gen] or with 
+    [gen]=[finish_gen], the function will run forever, or until the system 
+    is overloaded. *)
 let sublist start_gen finish_gen tdists_llist =
   LL.take_while (fun tds -> tds.gen <= finish_gen)
                 (LL.drop_while (fun tds -> tds.gen < start_gen)
 		               tdists_llist)
 
-let selection generations tdists_llist =
-  let add_if_selected td gens_plus_tds =
-	let gens, tds = gens_plus_tds in
-	if td.gen = (L.hd generations)
-        then (L.tl generations), (LL.cons td tds)
-	else generations, tds
-  in
-  let empty_generations, new_tdists_llist = 
-	LL.fold_right add_if_selected (generations, LL.nil) tdists_llist in
-  new_tdists_llist
-
-    
+(** In [select_gens generations tdists_llist], [generations] is a lazy
+    list of integers in increasing order, and [tdists_llist] is a lazy
+    list of tdists.  The function returns a lazy list contanining those 
+    tdists whose generation numbers match the integers in [generations]. *)
 let select_gens generations tdists_llist =
   let open LL in
-  let rec select gens tds =
-    Printf.printf "%d %d\n" (hd gens) (hd tds);
-    if is_empty tds then tds
-    else let g, td = hd gens, hd tds in
-         if g = td.gen
-         then cons td (select (tl gens) (tl tds))
-         else (select (tl gens) (tl tds))
+  let rec select gs tds =
+    if is_empty tds || is_empty gs then nil
+    else 
+    let g, td = hd gs, hd tds in
+    let tdg = td.gen in
+    if g = tdg then cons td (select (tl gs) (tl tds))
+    else if g > tdg then select gs (tl tds) (* let tds catch up *)
+    else select (tl gs) tds                 (* let gs catch up *)
   in select generations tdists_llist
