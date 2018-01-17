fa6604d2d377c1b8219915189b03ec2dcb34568b
mars0i <mabrams@uab.edu>
2018-01-17 11:38:56 -0600

    fixed typo in doc comment

--- src/lib/models/tdists.ml
+++ src/lib/models/tdists.ml
@@ -18,7 +18,7 @@ let add_gens ?(first_tick=0) dists_llist =
 
 let remove_gens tdists_llist = LL.map dists tdists_llist
 
-(** [tdists_sublist start_t finish_t tdists_llist] returns a lazy list that's
+(** [sublist start_t finish_t tdists_llist] returns a lazy list that's
     a finite sublist of [tdists_llist], from the element with [gen]=[start_gen]
     to the element with [gen]=[finish_gen], inclusive.  Note that if the list
     is infinite and there is no element with [gen]=[start_gen] or with 
